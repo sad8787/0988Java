c93f9fc9edd9263fa32ab25a11950017e952c178
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-07-30 11:47:53 +0300

    Create Main.java

---
+++ Task2.5/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter data number ");
+        int a= scanner.nextInt();
+        if(a==10)
+            System.out.println("True");
+        else
+            System.out.println("False");
+
+    }
+}
