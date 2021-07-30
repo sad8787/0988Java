c5a4113838af2bd5c1d14ba259ca8ed881571ae1
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-07-30 11:19:30 +0300

    Create Main.java

---
+++ Task2.3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+      for (int i=1;i<11;i++){
+          for (int j=0;j< i;j++){
+              System.out.print("8\t");
+          }
+          System.out.print("\n");
+
+      }
+
+    }
+}
