e38fe838dd4ad7cbb7d2190a597289ba42528f1f
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-08-02 09:55:13 +0300

    Create Main.java

---
+++ Task3.2/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println (min (-20, -10));
+        System.out.println (min (-40, -10, -30, 40));
+        System.out.println (min (-20, -40, -30, 40));
+        System.out.println (min (-20, -10, -40, 40));
+        System.out.println (min (-20, -10, -30, -40));
+
+    }
+    public static int min(int a, int b, int c, int d) {
+        a=min(a,b);
+        a=min(a,c);
+        a=min(a,d);
+        return a;
+    }
+
+    public static int min(int a, int b) {
+        if(a>b){a=b;}
+        return a;
+    }
