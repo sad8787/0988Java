edcbf57af1a5301c01fd139662c5a33418a9504d
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-08-03 23:57:22 +0300

    Create Family.java

---
+++ Task_5.2/Family.java
@@ -0,0 +1,16 @@
+public class Family {
+    private String family;
+    private String citie;
+
+    public Family(String family,String citie){
+        this.family=family;
+        this.citie=citie;
+    }
+    public String getFamily() {
+        return family;
+    }
+    public String getAddres(){
+        return citie;
+    }
+
+}
