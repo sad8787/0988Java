dec28de3cc48af518e5f757ab96c19e67f22d29d
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-08-03 19:36:37 +0300

    Create Main.java

---
+++ Task_5.1/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList();
+        names.add("Анатолий");
+        names.add("Георгий");
+        names.add("Руслан");
+        names.add("Георгий");
+        names.add("Павел");
+        names.add("Руслан");
+        names.add("Анатолий");
+        names.add("Георгий");
+        names.add("Руслан");
+        names.add("Георгий");
+        names.add("Павел");
+        names.add("Руслан");
+        ArrayList<String> namesFinal = new ArrayList();
+        for(String name :names)
+        {
+            if(!namesFinal.contains(name))
+            {
+                namesFinal.add(name);
+                System.out.println(name);
+            }
+        }
+
+    }
+
+}
