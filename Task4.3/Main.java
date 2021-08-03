e5732a8f3d450618ae7e7be81a5d8ffbc2367c6f
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-08-03 12:07:26 +0300

    Update Main.java

--- Task4.3/Main.java
+++ Task4.3/Main.java
@@ -1,1 +1,28 @@
-j
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1=new Cat();
+        cat1.age=3;
+        cat1.strength=5;
+        cat1.weight=3;
+        Cat cat2=new Cat();
+        cat2.age=2;
+        cat2.strength=5;
+        cat2.weight=3;
+        System.out.println("Cat 1 challenges Cat 2 to a fight. is the victor? "+cat1.fight(cat2));
+        System.out.println("Cat 2 challenges Cat 1 to a fight. is the victor? "+cat2.fight(cat1));
+
+    }
+}
+
+ class Cat{
+    public int weight, age, strength;
+    public Cat(){
+
+    }
+    public int power(){
+        return (weight*strength)*3-age;
+    }
+    public boolean fight(Cat cat){//
+        return (power()>= cat.power());
+    }
+}
