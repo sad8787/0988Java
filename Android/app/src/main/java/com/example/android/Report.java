854effdb8d02120c7c6033c8c194180ac9818811
sad8787 <sadielgodales@gmail.com>
2021-08-23 09:34:27 +0300

    d
    d

--- Android/app/src/main/java/com/example/android/Report.java
+++ Android/app/src/main/java/com/example/android/Report.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class Report extends AppCompatActivity {
-    int correctAnswers;
-    int numberofquestions;
-    Question[] questions;
+    int val;
+    int length;
+    String report;
     TextView textViewReport;
+    TextView porcent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
+        val=getIntent().getIntExtra("val",0);
+        length=getIntent().getIntExtra("length",0);
+        report=getIntent().getStringExtra("report");
+        textViewReport=findViewById(R.id.textViewReport);
+        porcent=findViewById(R.id.porcent);
+        textViewReport.setText(report);
+        double a=(100*val)/length;
+        DecimalFormat df = new DecimalFormat("#.00");
+        porcent.setText("The exam was completed in a "+df.format(a)+"%");
 
     }
 }
